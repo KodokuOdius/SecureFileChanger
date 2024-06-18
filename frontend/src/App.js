3f4c5cef695bf7c59ded4f055420cab187855621
KodokuOdius <alanfoxincoolctranica@gmail.com>
2024-06-19 00:41:41 +0300

    mod: new build

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -18,7 +18,7 @@ export const FilesIcons = {
 }
 
 export const APIServer = {
-    serverHost: "http://localhost:8080/api",
+    serverHost: "http://82.97.248.164:8080/api",
     tokenName: "COMPANYCLOUD_TOKEN",
     auth: {
         register: "/auth/register",
